41c71d5bd5a06f26cdb8859a41dc067280aca7a6
Augustus Odena <aqo2@columbia.edu>
2014-05-22 17:48:57 -0400

    not done with this yet

---
+++ milk/program.cpp
@@ -0,0 +1,33 @@
+/*
+ID: odenaau1
+PROG: milk
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+	ofstream fout ("milk.out");
+    	ifstream fin ("milk.in");
+    	int N, S;
+    	fin >> N >> S;
+	/*
+	read in each tuple
+	sort a vector 
+	*/
+	
+}
+
+
+
+
+
+
+
+
