992588403e516629d65068fc86c74bf7acbc6e98
Augustus Odena <aqo2@columbia.edu>
2014-05-27 17:12:29 -0400

    finished the milk greedy one

--- milk/program.cpp
+++ milk/program.cpp
@@ -7,21 +7,41 @@ LANG: C++
 #include <fstream>
 #include <string>
 #include <vector>
+#include <utility>
+#include <algorithm>
 
 using namespace std;
 
-
+bool cmp(pair<int,int> a, pair<int,int> b){
+	return a.first < b.first;
+}
 
 int main() {
 	ofstream fout ("milk.out");
     	ifstream fin ("milk.in");
-    	int N, S;
-    	fin >> N >> S;
-	/*
-	read in each tuple
-	sort a vector 
-	*/
-	
+    	int total_milk, farmer_count;
+    	fin >> total_milk >> farmer_count;
+	int i, px, qty;
+	vector< pair<int,int>  > market;
+	for (i = 0; i < farmer_count; i++){
+		fin >> px >> qty;
+		market.push_back(make_pair(px, qty));
+	}
+	sort(market.begin(), market.end(), cmp);
+	int total_cost = 0;
+	i = 0;
+	while(total_milk > 0){
+		if (market[i].second >= total_milk){
+			total_cost += total_milk * market[i].first;
+			total_milk = 0;	
+		}	
+		else{
+			total_cost += market[i].first * market[i].second;
+			total_milk -= market[i].second;
+		}
+		i++;
+	}
+	fout << total_cost << endl;;
 }
 
 
