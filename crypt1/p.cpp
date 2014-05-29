370ae874f9148014b6b90d7e7060f04dbc4d87ce
Augustus Odena <aqo2@columbia.edu>
2014-05-29 17:19:05 -0400

    eod the 29th

--- crypt1/p.cpp
+++ crypt1/p.cpp
@@ -1,6 +1,6 @@
 /*
 ID: odenaau1
-PROG: calfflac
+PROG: crypt1
 LANG: C++
 */
 #include <iostream>
@@ -12,104 +12,41 @@ LANG: C++
 #include <locale>
 #include <ctype.h>
 
-
 using namespace std;
 
 
 
 int main() {
-	ofstream fout ("calfflac.out");
-    	ifstream fin ("calfflac.in");
-    	char buffer;
-	int i, j, k, p, len, current, max, 
-		start, finish, new_j, new_k, parity;
-	vector<char> chrs, final;
-    	while (fin >> noskipws >> buffer){
-		chrs.push_back(tolower(buffer));
-		final.push_back(buffer);
+	ofstream fout ("crypt1.out");
+    	ifstream fin ("crypt1.in");
+	int i, set_size, buffer;  
+	vector<int> ints;
+	fin >> set_size;
+    	for(i = 0; i < set_size; i++){
+		fin >> buffer;
+		ints.push_back(buffer);
 	}
-	len = chrs.size();
-	//find the longest even palindrome
-	
-	//init the length placeholders
-	current = 0;
-	max = 0;
-	//for every letter
-	for(i = 0; i < len; i++){
-		//if (i ==  23)
-		//	cout << "index: " <<  i << " char: " << chrs[i] << endl;
-		if (! isalpha(chrs[i])){continue;}
-		j = i; //start both counters at i
-		k = i;
-		current = 1;
-		//while we are within bounds and both sides the same
-		while(j > 0 && k < (len - 1) && chrs[j] == chrs[k]){
-			new_j = j - 1;
-			new_k = k + 1;
-			while( (! isalpha(chrs[new_j])) && new_j >= 0 ){new_j--;}
-			while( (! isalpha(chrs[new_k])) && new_k <= (len - 1)){new_k++;}
-			//if (i == 23)
-			//	cout << new_j << " " << new_k << endl;
-			if(chrs[new_j] == chrs[new_k]){
-				j = new_j;
-				k = new_k;
-				current += 2;
-			}
-			else{
-				break;
-			}
-
-		}
-		if (current > max){ //then update max and the pointers
-			max = current;
-			start = j;
-			finish = k;
-			parity = 1;
+	int a,b,c,d,e;
+	for(a = 0; a < set_size; a++){
+		if (ints[a] == 0)
+			continue
+		for(b = 0; b < set_size; b++){
+			for (c = 0; c < set_size; c++)
+				if (ints[c] == 0)
+					continue
+				for(d = 0; d < set_size; d++){
+					for(e = 0; e < set_size;e++){
+						//do out the multiplication here
+						//then filter
+					}
+				}
 		}
 	}
-	
-	//find the longest odd palindrome	
-	//for every pair of 2 letters
-	for(i = 0; i < len - 1; i++){
-		j = i;
-		k = i + 1;
-		while( (! isalpha(chrs[k])) && k < len){
-			k++;
-		}
-		if (k == len){continue;}
-		current = 2;
-		if (chrs[j] != chrs[k]){continue;}
-
-		while(j > 0 && k < (len - 1) && chrs[j] == chrs[k]){
-			new_j = j - 1;
-			new_k = k + 1;
-			while( (! isalpha(chrs[new_j])) && new_j >= 0 ){new_j--;}
-			while( (! isalpha(chrs[new_k])) && new_k <= (len - 1)){new_k++;}
-			if(chrs[new_j] == chrs[new_k]){
-				j = new_j;
-				k = new_k;
-				current += 2;
-			}
-			else{
-				break;
-			}
-
-		}	
-	
-		if (current > max){
-			max = current;
-			start = j;
-			finish = k;
-			parity = 0;
-		}
-	}	
-	//finish up
-	cout << "parity: " << parity << endl;
-	fout << max << endl;
-	for (i = start; i <= finish; i++){
-		fout << final[i];
+	cout << "the set has size: " << set_size << endl; 
+	for(i = 0; i < set_size; i++){
+		cout << ints[i] << " "; 
 	}
-	fout << endl;
+	cout << endl;
 }
 
 
